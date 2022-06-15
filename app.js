a761f40fcf2837485eceba0992e72c313135f208
Christopher Dazzo <dazzo.chris@gmail.com>
2022-06-15 09:51:40 -0500

    wip: adding items

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-function alert(){
-    console.log('hello working');
-}
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
-alert()
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions 
+function addTodo(e) {
+    e.preventDefault();
+    // Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create Li
+    const newTodo = document.createElement('li');
+    newTodo.classList.add('todo-item');
+    newTodo.innerText = todoInput.value;
+    todoDiv.appendChild(newTodo);
+    // Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('completed-btn');
+    todoDiv.appendChild(completedButton);
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    // clear the input
+    todoInput.value = '';
+}
